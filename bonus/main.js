7953fa7178ad8f82ffa5effde6c4c7bb93883839
EnricoCiccolini <piroroma90@gmail.com>
2025-03-13 14:52:59 +0100

    feat : add button and event list

--- bonus/main.js
+++ bonus/main.js
@@ -11,27 +11,52 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 const mailElement = document.getElementById('mail')
 
+const buttonElement = document.getElementById('button')
+
 const RandMail = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
 
 
-let  mail = ''
+
 
 
 tenRandommail(RandMail)
 
+
+buttonElement.addEventListener('click', function () {
+tenRandommail(RandMail)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function tenRandommail (array){
 
-
+let  mail = ''
     for(let i = 0 ; i< 10 ; i++){
 
 
 
         axios.get(RandMail).then((response) =>{
+
+
+            console.log(response.data.response)
               
         mail = mail + `<li class="list-group-item">${response.data.response}</li>` 
          
-         console.log(mail)     
+        // console.log(mail)     
         mailElement.innerHTML = mail
             
         } )
@@ -46,6 +71,8 @@ function tenRandommail (array){
 
     
 }
+
+
 
 
 
