4a49a957dd2ecc62ca86cfe077a8174bd6ffcd26
EnricoCiccolini <piroroma90@gmail.com>
2025-03-13 14:11:22 +0100

    feat: add email in html

--- main.js
+++ main.js
@@ -7,11 +7,40 @@ https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
+
+
+const mailElement = document.getElementById('mail')
+
 const RandMail = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
+
+
+let  mail = ''
+
+for(let i = 0 ; i< 10 ; i++){
+
+
+
+
 axios.get(RandMail).then((response) =>{
-    console.log(response)
+    
+    
+mail = mail + `<div> ${response.data.response} </div>` 
+  
+       console.log(mail)
+
+
+
+      
+mailElement.innerHTML = mail
+    
 } )
 .catch(error => {
     console.log("errore ")
-})
+})    
+
+
+
+}
+
+
